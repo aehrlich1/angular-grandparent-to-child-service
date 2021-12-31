d7e4cd248c0e787f118997ec9695e4c695b8b9fa
aehrlich1 <anatol.ehrlich@gmail.com>
2021-12-31 17:45:16 +0100

    added child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GrandparentComponent } from './grandparent/grandparent.component';
 import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GrandparentComponent,
-    ParentComponent
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
