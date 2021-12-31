77fc27fb3464988ae8bb0d861fffbdec12378cdb
aehrlich1 <anatol.ehrlich@gmail.com>
2021-12-31 17:41:46 +0100

    added grandparent component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GrandparentComponent } from './grandparent/grandparent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrandparentComponent
   ],
   imports: [
     BrowserModule
